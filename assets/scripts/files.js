95f265e293fcb57d8370636110c172da8f3954cd
Mike Bédard <mike01bedard@gmail.com>
2023-03-05 17:35:01 -0500

    Integrate carte

--- assets/scripts/files.js
+++ assets/scripts/files.js
@@ -1,7 +1,7 @@
 const fileURLs = [
-
+ 'https://raw.githubusercontent.com/Demomaker/UnofficialExpressLotbiniere/main/assets/files/mapExpressSainteFoy.png'
 ];
 
 const fileTitles = {
-    
+ 'https://raw.githubusercontent.com/Demomaker/UnofficialExpressLotbiniere/main/assets/files/mapExpressSainteFoy.png': 'Carte non-officielle du service Express vers Sainte-Foy'
 }
